50bd1e8a054da59affca520ebf04a1717265ff52
bryt0 <85228422+bryt0@users.noreply.github.com>
2021-06-29 11:18:45 +0100

    Update evenlySpaced.js

--- backend-weekly-task-main/backend-weekly-task-main/src/abdulkadir-munir/evenlySpaced.js
+++ backend-weekly-task-main/backend-weekly-task-main/src/abdulkadir-munir/evenlySpaced.js
@@ -1,14 +1,12 @@
 
+
+
 function evenlySpaced(a, b, c) {
-  
-     if ((a - b === b - c) || (a - c === c - b) || (a - b === c - a)) {
-        return true;
-    } else {
-        return false;
-    }
+    let number;
+      number = ((c+a)/2);
+  return number == b?true:false
 }
 
 
 console.log(evenlySpaced(2,4,6)) 
 console.log (evenlySpaced(3,6,12)) 
-
